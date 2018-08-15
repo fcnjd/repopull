2ce52c3674b7b9e961f2aa5dea18205b797ab077
Julian Dreykorn <dreykorn.julian@gmail.com>
2018-08-15 11:50:43 +0200

    new: a log file will be generated at the current directory

--- pull-all-git-repos.sh
+++ pull-all-git-repos.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+FILEPATH=$PWD;
+LOGPATH="$FILEPATH"/pull-all-git-repos.log;
+#echo "$LOGPATH";
 for repo in */.git
 do
     cd $repo/..;
-    echo "$repo";
-    git pull;
-    cd ..
+    echo "$repo" >> "$LOGPATH" 2>&1;
+    git pull >> "$LOGPATH" 2>&1;
+    cd ..;
 done
-echo "all git repositories in this folder have been pulled"
+echo "all git repositories in this folder have been pulled. Please read the logfile located in $LOGPATH for more information.";
